f9b7771e4ec1c52621a98571d3b193b8a476b63b
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-01-17 00:37:27 -0300

    Iniciando o projeto

---
+++ ControleDeContato/ControleDeContato/Controllers/ContactController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContato.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Edit()
+        {
+            return View();
+        }
+        public IActionResult DeleteContact()
+        {
+            return View();
+        }
+    }
+}
