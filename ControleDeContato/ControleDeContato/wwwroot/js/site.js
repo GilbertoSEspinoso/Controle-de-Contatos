8a2ae2d481d93f3fa5a4dc40e047a90cf52c5851
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-01-17 20:25:24 -0300

    Criando Paginação / Filtro de Busca

--- ControleDeContato/ControleDeContato/wwwroot/js/site.js
+++ ControleDeContato/ControleDeContato/wwwroot/js/site.js
@@ -2,6 +2,39 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $('#table-contacts').DataTable({
+        "ordering": true,
+        "paging": true,
+        "searching": true,
+        "oLanguage": {         //tradução da tabela com Jquery
+            "sEmptyTable": "Nenhum registro encontrado na tabela",
+            "sInfo": " _START_ até _END_ de _TOTAL_ ",
+            "sInfoEmpty": " 0 até 0 de 0 Registros",
+            "sInfoFiltered": "(Filtrar de _MAX_ total )",
+            "sInfoPostFix": "",
+            "sInfoThousands": ".",
+            "sLengthMenu": "Mostrar _MENU_ registros por pagina",
+            "sLoadingRecords": "Carregando...",
+            "sProcessing": "Processando...",
+            "sZeroRecords": "Nenhum registro encontrado",
+            "sSearch": "Pesquisar",
+            "oPaginate": {
+                "sNext": "Próximo",
+                "sPrevious": "Anterior",
+                "sFirst": "Primeiro",
+                "sLast": "Último"
+            },
+            "oAria": {
+                "sSortAscending": ": Ordenar colunas de forma ascendente",
+                "sSortDescending": ": Ordenar colunas de forma descendente"
+            }
+        }
+    });
+});
+
+
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
+
