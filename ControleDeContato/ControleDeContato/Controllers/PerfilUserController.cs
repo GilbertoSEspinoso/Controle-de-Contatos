9d5048290c90ec19c7c3db7b0025957a81e9ad5b
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-01-21 15:05:23 -0300

    add perfilUser

---
+++ ControleDeContato/ControleDeContato/Controllers/PerfilUserController.cs
@@ -0,0 +1,15 @@
+﻿using ControleDeContato.Filters;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContato.Controllers
+{
+    public class PerfilUserController : Controller
+    {
+
+        [PaginaUsuarioLogado]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
